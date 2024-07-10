06ad9ff0028a91557fb280eb8d04281e2fb4d104
Joseph Keating <44977242+evo-palaeo@users.noreply.github.com>
2024-07-10 13:38:54 +0100

    Initial upload
    Dummy occ data and PBDB vignette

---
+++ TeachingMaterials/R/TeachingMaterials.r
@@ -0,0 +1,23 @@
+###################### TeachingMaterials ###################### 
+################### J. N. Keating, 10/07/24 ###################
+
+################### Data ###################
+
+#' Simulated occurence dataset
+#'
+#' A simulated dataset of fossil occurences analagous to data dowloaded from the PBDB. 
+#'
+#' @docType data
+#'
+#' @usage data(sim_occ)
+#'
+#' @format data.frame
+#'
+#' @keywords datasets
+#'
+#' @references Kocsis, Ádám T., Carl J. Reddin, John Alroy, and Wolfgang Kiessling. "The R package divDyn for quantifying diversity dynamics using fossil sampling data." Methods in Ecology and Evolution 10, no. 5 (2019): 735-743.
+#'  
+#' @examples
+#' data(sim_occ)
+#' head(sim_occ)
+"sim_occ"
