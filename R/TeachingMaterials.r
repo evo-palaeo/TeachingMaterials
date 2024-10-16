bde918e98706eddcc4af58d5d69a8643bbdcb050
evo-palaeo <joekeating99@googlemail.com>
2024-10-16 22:44:05 +0100

    theropods

--- R/TeachingMaterials.r
+++ R/TeachingMaterials.r
@@ -3,9 +3,9 @@
 
 ################### Data ###################
 
-#' Simulated occurence dataset
+#' Simulated occurrence dataset
 #'
-#' A simulated dataset of fossil occurences analagous to data dowloaded from the PBDB. 
+#' A simulated dataset of fossil occurrences analagous to data downloaded from the PBDB. 
 #'
 #' @docType data
 #'
@@ -20,4 +20,47 @@
 #' @examples
 #' data(sim_occ)
 #' head(sim_occ)
+#' @export
 "sim_occ"
+
+
+#' Theropod phylogenetic tree 
+#'
+#' A Maximum Clade Credibility tree of theropods modified from Xu et al. 2023.
+#'
+#' @docType data
+#'
+#' @usage data(theropod_tree)
+#'
+#' @format phylo
+#'
+#' @keywords datasets
+#'
+#' @references Xu, L., Wang, M., Chen, R., Dong, L., Lin, M., Xu, X., Tang, J., You, H., Zhou, G., Wang, L. and He, W., 2023. A new avialan theropod from an emerging Jurassic terrestrial fauna. Nature, 621(7978), pp.336-343.
+#' 
+#' @examples
+#' data(theropod_tree)
+#' plot(theropod_tree, cex = 0.5)
+#' @export
+"theropod_tree"
+
+
+#' Theropod stratigraphic data 
+#'
+#' The First Apperance Datum and Last Appearance Datum for theropod dinosaurs from Xu et al. 2023.
+#'
+#' @docType data
+#'
+#' @usage data(theropod_ages)
+#'
+#' @format data.frame
+#'
+#' @keywords datasets
+#'
+#' @references Xu, L., Wang, M., Chen, R., Dong, L., Lin, M., Xu, X., Tang, J., You, H., Zhou, G., Wang, L. and He, W., 2023. A new avialan theropod from an emerging Jurassic terrestrial fauna. Nature, 621(7978), pp.336-343.
+#' 
+#' @examples
+#' data(theropod_ages)
+#' head(theropod_ages)
+#' @export
+"theropod_ages"
